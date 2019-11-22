1ed9d265a6ab86095eaea81306659336bfc0c728
AMH <amh@AMHs-MacBook-Air.local>
2019-11-21 21:40:18 -0700

    a professional message conveying my hireability

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,42 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>{{ name }}</h2>
+    <h3>{{ fullName }}</h3>
+    <button @click="promote">Promote</button>
+    <button v-if="pageNum>0" @click="last">Last</button>
+    <button v-if="pageNum<keypages.length-1" @click="next">Next</button>
+    <button v-if="pageNum==keypages.length-1" @click="next">SECRET</button>
+    <input v-model="name" />
+    <pre>{{ keypages[pageNum] }}</pre>
+    <pre>{{ pageNum }}</pre>
   </div>
 </template>
 
 <script>
+import pages from '../text.json'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    promote: function () {
+      this.title = Math.random()
+
+    },
+    next: function() { this.pageNum++ },
+    last: function() { this.pageNum-- },
+  },
+  data: () => ({
+    name: 'Bob',
+    title: 'King',
+    pageNum: 0,
+    keypages: pages
+  }),
+  computed:  {
+    fullName: function()  {return this.name + ' ' + this.title}
   }
 }
 </script>
